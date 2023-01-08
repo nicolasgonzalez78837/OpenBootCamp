52f5ede1f671cab4cca5567d5fccdd287a6a8933
Javier Nicolás González <89177014+nicolasgonzalez78837@users.noreply.github.com>
2023-01-08 19:43:43 -0300

    Create python7.py

---
+++ python7.py
@@ -0,0 +1,20 @@
+def matematicas(a, b):
+   def suma(a, b):
+      print(a + b)
+
+   def resta(a, b):
+      print(a - b)
+        
+   def multi(a, b):
+      print(a * b)
+
+   def divi(a, b):
+      print(a / b)
+
+   suma(a, b)
+   resta(a, b)
+   multi(a, b)
+   divi(a, b)
+    
+    
+matematicas(60,15)
